4dce359405bbd67ac2d7c278e78b8c54dfd5516c
Nathan <nathan.turlure@epsi.fr>
2022-04-18 16:49:38 +0200

    Ajout du calcul pour l'age

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -42,31 +42,27 @@ class FormController extends AbstractController
     #[Route('/formFormations', name: 'app_formFormations')]
     public function formations(Request $request, MailerInterface $mailer ): Response
     {
+        $form2 = $this->createForm(FormationType::class);
+        $dateOfBirth = date($_GET["Date_Naissance"]);
+        $today = date("d-m-Y");
+        
+        $diff = date_diff(date_create($dateOfBirth), date_create($today));
+        $days = $diff->days;
+        $age = round($days /365);
 
         if($request->isMethod('POST')){
             $form2->handleRequest($request);
             
-            $email = (new TemplatedEmail())
-                ->from('fima_tech@outlook.com')
-                ->to("ar.lefebvre62@gmail.com")
-                ->subject('Contact')
-                ->htmlTemplate('emails/email.html.twig')
-                ->context([
-                    'Nom' => $_GET["Nom"],
-                    'Prenom' => $_GET["Prenom"],
-                    'Adresse' => $_GET["Adresse"],
-                    'Formation' => $form2->get('Formation')->getData(),
-                ]);
-            $mailer->send($email);
             return $this->redirectToRoute('app_form');
-            
         }
+        
         return $this->render('form/formations.html.twig', [
             'form2' => $form2->createView(),
             'Nom' => $_GET["Nom"],
             'Prenom' => $_GET["Prenom"],
             'Adresse' => $_GET["Adresse"],
             'Date_Naissance' => $_GET["Date_Naissance"],
+            'Age' => $age,
         ]);
     }
 
